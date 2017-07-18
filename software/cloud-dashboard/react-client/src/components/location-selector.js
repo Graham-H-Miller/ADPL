c1c7111d0240a3c1babe9ae806c0fe22b88cc729
Cal Nightingale <cal.nightingale@gmail.com>
2017-07-18 15:35:47 -0400

    [WIP] default data

--- software/cloud-dashboard/react-client/src/components/location-selector.js
+++ software/cloud-dashboard/react-client/src/components/location-selector.js
@@ -27,7 +27,7 @@ class LocationSelector extends Component {
 	render() {
 		// set all necessary info to the first location
 		//this.setState({currentValue: this.locationData[0]})
-        //this.props.fetchTemps(this.locationData[0], this.props.meta.daysToFetch || constants.daysToFetch);
+        this.props.fetchTemps(this.locationData[0], this.props.meta.daysToFetch || constants.daysToFetch);
         this.props.selectLocation(this.locationData[0]);
 
 		return (
