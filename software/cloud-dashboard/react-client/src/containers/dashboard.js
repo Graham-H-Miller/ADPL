75505ef5266d2c42105a4787c7f0ed0990f7a644
Cal Nightingale <cal.nightingale@gmail.com>
2017-07-20 10:08:58 -0400

    Reverted to master code

--- software/cloud-dashboard/react-client/src/containers/dashboard.js
+++ software/cloud-dashboard/react-client/src/containers/dashboard.js
@@ -8,61 +8,66 @@ import {Tab, Tabs} from 'react-toolbox';
 import LocationSelector from '../components/location-selector';
 import TempView from '../components/temp-view';
 import './dashboard.css';
-//import { getCoreID } from "node-server/config/device-map"
 
 class Dashboard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			tabIndex: 0,
-		}
-		this.props.fetchLocations();
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            tabIndex: 0,
+        }
+        this.props.fetchLocations();
+    }
 
-	handleTabChange = index => {
-		this.setState({tabIndex: index});
-	}
-	// TODO: Refactor LocationSelector and TempView into containers 
-	render() {
-		return (
+    handleTabChange = index => {
+        this.setState({tabIndex: index});
+    }
+    // TODO: Refactor LocationSelector and TempView into containers
+    render() {
+        return (
 			<div className="dashboard">
-				<LocationSelector 
-					locations={this.props.locations} 
+				<LocationSelector
+					locations={this.props.locations}
 					selectLocation={this.props.selectLocation}
 					fetchTemps={this.props.fetchTemps}
 					meta={this.props.meta}/>
-				{
-					this.props.currentLocation &&
-					//<h1>Current Loc: {this.props.currentLocation}</h1> &&
-					<TempView
-						temps={this.props.temps}
-						fetchTemps={this.props.fetchTemps}
-						setDaysToFetch={this.props.setDaysToFetch}
-						currentLocation={this.props.currentLocation}/> &&
-					<BucketView />
-				}
+                {
+                    this.props.currentLocation &&
+					<Tabs index={this.state.tabIndex} onChange={this.handleTabChange}>
+						<Tab label="Temps">
+							<TempView
+								temps={this.props.temps}
+								fetchTemps={this.props.fetchTemps}
+								setDaysToFetch={this.props.setDaysToFetch}
+								currentLocation={this.props.currentLocation}/>
+						</Tab>
+						<Tab label="Bucket Tips">
+							<BucketView />
 
-				<div className="links"> 
-					<a href="http://sanitation.pratt.duke.edu/onsite-treatment/about-onsite-treatment-project" target="_blank"> ADPL Project </a> | 
+						</Tab>
+					</Tabs>
+                }
+
+				<div className="links">
+					<a href="http://sanitation.pratt.duke.edu/onsite-treatment/about-onsite-treatment-project" target="_blank"> ADPL Project </a> |
 					<a href="https://github.com/mlp6/ADPL" target="_blank"> Github </a> |
 					Web by <a href="http://suyashkumar.com" target="_blank"> Suyash Kumar </a>
 				</div>
 			</div>
-		)
-	} 
+        )
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-	return bindActionCreators({ fetchTemps, fetchLocations, selectLocation, setDaysToFetch}, dispatch);
+    return bindActionCreators({ fetchTemps, fetchLocations, selectLocation, setDaysToFetch}, dispatch);
 }
 
 const mapStateToProps = state => {
-	return { 
-		temps: state.temps,
-		locations: state.locationData.locations,
-		currentLocation: state.locationData.currentLocation,
-		meta: state.meta,
-	}
+    return {
+        temps: state.temps,
+        locations: state.locationData.locations,
+        currentLocation: state.locationData.currentLocation,
+        meta: state.meta,
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
